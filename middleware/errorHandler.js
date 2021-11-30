8b95622826c82cda289a12b90e1538d2731d4c57
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-11-30 17:58:17 +0200

    Create errorHandler.js

---
+++ middleware/errorHandler.js
@@ -0,0 +1,16 @@
+exports.unknownEndpoint = (req, res, next) => {
+    // if i got to this middleware then i missed endpoint
+    res.status(404).json({ error: "unknown endpoint" });
+    next();
+};
+
+exports.errorHandlerMiddleware = (err, req, res, next) => {
+    if (err.status) {
+      res.status(err.status);
+      res.send({ error: err.message });
+    } else {
+      console.log(err);
+      res.status(500);
+      res.send({ error: 'Internal Server Error' });
+    }
+};
