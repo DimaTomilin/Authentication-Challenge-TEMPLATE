d08f692dc8eb8e88523b982587379dbf8d78e9f9
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-11-30 18:43:47 +0200

    test 1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {
     unknownEndpoint,
   } = require('./middleware/errorHandler');
 
-const USERS = [{ email: "admin@email.com", name: "admin", password: "$2b$10$KtkEC/cbvQmpctuRTlZxA.8eoORm7vVmdxSG1GqnwW.wJ9uvQOTQu", isAdmin: true }]
+const USERS = [{ email: "admin@email.com", name: "admin", password: "$2b$10$KtkEC/cbvQmpctuRTlZxA.8eoORm7vVmdxSG1GqnwW.wJ9uvQOTQu", isAdmin: true },]
 const INFORMATION = [{email:"1111", info:"23213"}]
 const REFRESHTOKENS = []
 
@@ -23,7 +23,22 @@ app.use(
   morgan(' :method :url :status :res[content-length] - :response-time ms :body')
 );
 
-app.post("/users/register", registrationUser)
+app.post("/users/register", async (req, res) => {
+    try{
+        const {email,user,password} = req.body
+        if(USERS.find(user => user.name===req.body.user)){
+            return res.status(409).send("user already exists")
+        }
+        const hashedPassword = await encryption(password)
+        const newUser = { email, name: user, password: hashedPassword, isAdmin: false }
+        USERS.push(newUser)
+        INFORMATION.push({email, info:`${user} info`})
+        res.status(201).send("Register Success")
+    } catch {
+        res.status(500).send("Server error")
+    }  
+})
+
 app.use(express.static("client/build"));
 
 app.use(unknownEndpoint);
--- controlers/Users.js
+++ controlers/Users.js
@@ -1,11 +1,12 @@
-const {USERS} = require("../app")
+
 const {encryption} = require('../helpers/bcrypt')
 
 exports.registrationUser = async (req, res) => {
     const {email,user,password} = req.body
     const hashedPassword = await encryption(password)
     const newUser = { email, name: user, password: hashedPassword, isAdmin: false }
-    USERS.push(newUser)
-    console.log(USERS)
+   
+    //USERS.push(newUser)
+
     res.status(201).send("Register Success")
   };
