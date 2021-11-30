f383a59d93e7e3b6f56f695c8abd8d39b6c93abb
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-11-30 17:58:10 +0200

    Main things

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-/* write your server code here */
+const express = require("express");
+const app = express();
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const {encryption} = require('./helpers/bcrypt')
+const {registrationUser} = require("./controlers/Users")
+
+const {
+    errorHandlerMiddleware,
+    unknownEndpoint,
+  } = require('./middleware/errorHandler');
+
+const USERS = [{ email: "admin@email.com", name: "admin", password: "$2b$10$KtkEC/cbvQmpctuRTlZxA.8eoORm7vVmdxSG1GqnwW.wJ9uvQOTQu", isAdmin: true }]
+const INFORMATION = [{email:"1111", info:"23213"}]
+const REFRESHTOKENS = []
+
+app.use(express.json());
+
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(' :method :url :status :res[content-length] - :response-time ms :body')
+);
+
+app.post("/users/register", registrationUser)
+app.use(express.static("client/build"));
+
+app.use(unknownEndpoint);
+app.use(errorHandlerMiddleware);
+
+module.exports = {USERS};
+module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-/* write the code to run app.js here */
+const app = require('./app');
+
+const port = process.env.PORT || 8080;
+
+
+// starting the server
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
